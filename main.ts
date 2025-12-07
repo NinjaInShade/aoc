6f297038375fe26e7a61408ea3558fb5cd93a517
Leon Michalak <leonmichalak6@gmail.com>
2025-12-07 17:13:26 +0000

    Log challenge times

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import fsp from 'node:fs/promises';
 import path from 'node:path';
+import { performance } from 'node:perf_hooks';
 
 const now = new Date();
 
@@ -17,8 +18,16 @@ if (process.env.DEBUG) {
     console.log(`Input: ${input}`);
 }
 
-const part1Result = await part1(input);
-console.log(`Part 1: ${part1Result}`);
+{
+    const t0 = performance.now();
+    const result = await part1(input);
+    const t1 = performance.now();
+    console.log(`Part 1: ${result} (${t1 - t0}ms)`);
+}
 
-const part2Result = await part2(input);
-console.log(`Part 2: ${part2Result}`);
+{
+    const t0 = performance.now();
+    const result = await part2(input);
+    const t1 = performance.now();
+    console.log(`Part 2: ${result} (${t1 - t0}ms)`);
+}
